6bf03634a035dbed2f3df940f826fa72f2cf46ce
Joyx013 <thejoyx1000@gmail.com>
2026-02-22 12:00:12 +0100

    launcher.bat

---
+++ assets/scripts/launcher.bat
@@ -0,0 +1,10 @@
+@echo off
+title Mortal Client Launcher
+color 05
+echo ========================================
+echo          MORTAL CLIENT STARTING
+echo ========================================
+echo.
+:: Lunedi' al PC cambieremo il percorso qui sotto
+start "" "C:\Percorso\Al\Tuo\Minecraft.exe"
+pause
